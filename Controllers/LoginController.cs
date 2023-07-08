a1eb52a08a9a1a2d798262c734add11b4cafefc9
rithikagangishetty <121901013@smail.iitpkd.ac.in>
2023-07-08 15:25:05 +0530

    honeycomb Architecture

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -12,32 +12,6 @@ namespace Calenderwebapp.Controllers
     [Route("[controller]")]
     public class LoginController : Controller
     {
-        //private readonly LoginServices _loginServices;
-        //public LoginController(LoginServices loginServices) =>
-        //    _loginServices = loginServices;
-
-        //[HttpPost]
-        //[Route("login")]
-        //public IActionResult Login(Connections userdata)
-        //{
-        //    var user = _loginServices.Login(userdata.EmailId);
-        //    if (user != null)
-        //    { return Ok(user); }
-        //    else
-        //    {
-
-
-        //        return NotFound();
-        //    }
-        //}
-        //[HttpPost]
-        //[Route("signup")]
-        //public IActionResult Signup(Connections userdata)
-        //{
-        //    var user = _loginServices.Signup(userdata);
-
-        //    return Ok(user);
-        //}
         private readonly LoginSupervisor _loginSupervisor;
         public LoginController(LoginSupervisor loginSupervisor)
         {
