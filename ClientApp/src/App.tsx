bb55a5fb598d4d9a68d7fb3bac22bd011462bb0e
rithikagangishetty <121901013@smail.iitpkd.ac.in>
2023-07-12 12:47:09 +0530

    URL modified

--- ClientApp/src/App.tsx
+++ ClientApp/src/App.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
-
 import Layout from './components/Layout';
 import Home from './components/Home';
-import { useState } from "react";
-import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './custom.css'
 import Connections from './components/Connections';
@@ -13,19 +10,20 @@ import CalendarPage from './components/ViewCalendar'
 function App() {
 
     return (
-
+       <Router>
             <Layout>
-                <Router>
+              
                     <Switch>
-                        <Route exact path="/" component={Login} />
-                        <Route path="/Connections/:id" component={Connections} />
-                        <Route path="/Home/:id" component={Home} />
-                        <Route path="/ReactApp/:id" component={ReactApp} />
-                    <Route path="/calendar/:id/:connectionId" component={CalendarPage} />
+                        <Route  exact path="/" component={Login} />
+                        <Route exact path="/Home/:id" component={Home} />
+                        <Route path="/Home/Connections/:id" component={Connections} />
+                    <Route path="/Home/ReactApp/:id" component={ReactApp} />
+                        <Route path="/Home/Connections/calendar/:id/:connectionId" component={CalendarPage} />
                     </Switch>
 
-                </Router>
-        </Layout> 
+
+            </Layout> 
+        </Router>
        
 
   
